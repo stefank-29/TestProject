0d0d09d94f2a6b1709244999fe79314c80c70f71
stefank-29 <stefankaraferovic@gmail.com>
2021-06-17 16:05:29 +0200

    project layout

---
+++ components/Page.js
@@ -0,0 +1,71 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import PropTypes from 'prop-types';
+
+const GlobalStyles = createGlobalStyle`
+    html{
+        font-size: 62.5%;
+        box-sizing: border-box;
+        --red: #e01812;
+        --orange: #ffa220;
+        --black: #393939;
+        --grey: #3A3A3A;
+        --lightblue: #BDC5E4;
+        --hoverBlue: #5c6fbc;
+        --purple: #262161;
+        --yellow: #ffc40e;
+        --gold: #FFCB3B;
+        --textGrey: #818181;
+        --textGrey2: #B7B7B7;
+        --lightGrey: #e1e1e1;
+        --offWhite: #ededed;
+        --backgroundGrey: #E8E8E8;
+        --lightGreen: #3BFF89;
+        --lightRed: #FF3B3B;
+        scroll-behavior: smooth;
+    }
+
+
+    body {
+        padding: 0;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+            Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        font-size: 1.5rem;
+        background-color: var(--backgroundGrey);
+
+    }
+    a {
+        color: inherit;
+        text-decoration: none;
+        :hover{
+            text-decoration: underline;
+        }
+    }
+
+    *, *:before, *:after {
+         box-sizing: inherit;
+    }
+
+
+
+`;
+
+const InnerStyles = styled.div`
+    margin: 0 auto;
+    min-height: calc(100vh - 70px);
+    max-width: 1200px;
+    padding: 3rem 0;
+`;
+
+export default function Page({ children }) {
+    return (
+        <div>
+            <GlobalStyles />
+            <InnerStyles>{children}</InnerStyles>
+        </div>
+    );
+}
+
+Page.propTypes = {
+    children: PropTypes.any,
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import Router from 'next/router';
+import Page from '../components/Page';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <Page>
+            <Component {...pageProps} />
+        </Page>
+    );
 }
 
-export default MyApp
+export default MyApp;
